0489397a7b4a83bcb4bc67626069e95ef0a5bfd2
Godson Atakpu <ikhide.atakpu@gmail.com>
2025-04-30 17:22:30 +0200

    chore: setup graph ql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      graphiql: true,
+    }),
+  ],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
